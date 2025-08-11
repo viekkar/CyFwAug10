dd1e58ac95105b3259582a40c1cd0fdef31bb447
Your Name <viekkar@gmail.com>
2025-08-11 16:11:38 +0530

    feat: add demo test suite for e2e testing with Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+
+    specPattern: 'cypress/e2e/**/*.js',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
---
+++ cypress/e2e/Steps/demo.js
@@ -0,0 +1,16 @@
+
+describe("Demo Test Suite", () => {
+  it("should run a demo test", () => {
+    cy.visit("https://www.flipkart.com/");
+    cy.contains("Best of Electronics").should("be.visible");
+  });
+});
+
+
+
+
+
+
+
+
+// LaptopsAsus ROG AllyMacBook Pro M2Premium LaptopASUS ROG Strix SCAR 16 (2023) Core i9 13th GenASUS ROG Zephyrus M16 (2023) Core i9 13th GenASUS Zenbook 14 OLED (2022)Realme Book Prime Core i5 11th GenMicrosoft Surface Go Pentium 128GBApple LaptopsAcer LaptopsLenovo LaptopsDell LaptopsAsus LaptopsHP LaptopsGaming Laptops2 in 1 LaptopsLaptopsDell latest laptops 2022HP latest laptops 2022Infinix INBook Y1 PlusSAMSUNG Galaxy Book312th Gen Intel Core Laptops
